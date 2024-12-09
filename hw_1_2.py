1843c2efb68d990b711ec865dce598130136fc79
sqwo9 <zuevivan423@gmail.com>
2024-12-09 13:31:53 +0300

    Update hw_1_2.py

--- hw_1_2.py
+++ hw_1_2.py
@@ -1,2 +1,11 @@
-a = input()
-print(a == a[::-1])
+def palindrome(s):
+    s = ''.join(c.lower() for c in s if c.isalnum())
+    
+    n = len(s)
+    
+    for i in range(n // 2):
+        if s[i] != s[n - 1 - i]:
+            return False
+
+    return True
+print(palindrome('barabashka'))
